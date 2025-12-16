2d4c770d8ecc1e6dc7377f823e4a010fcfb5c96d
MatsWiese <stamwiese@gmail.com>
2025-12-16 20:43:49 +0100

    Initial Commit

---
+++ amStizzleReboot/amStizzleRebootApp.swift
@@ -0,0 +1,17 @@
+//
+//  amStizzleRebootApp.swift
+//  amStizzleReboot
+//
+//  Created by Fred Erik on 16.12.25.
+//
+
+import SwiftUI
+
+@main
+struct amStizzleRebootApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
