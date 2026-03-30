1643681a6110806731f6465d4e3e9aa03b24f249
MatsWiese <stamwiese@gmail.com>
2026-03-30 22:37:53 +0200

    redesign EventRowView

--- amStizzleReboot/EventsListFeature/EventRowView.swift
+++ amStizzleReboot/EventsListFeature/EventRowView.swift
@@ -12,10 +12,10 @@ import SwiftUI
 
 //@Observable class EventRowModel {
 //  let logger = Logger(subsystem: "amStizzleReboot", category: "EventRowModel")
-//  
+//
 //  let event: Event
 //  let currentUser: EventAttendee
-//  
+//
 //  init(event: Event) {
 //    self.event = event
 //  }
@@ -38,13 +38,15 @@ import SwiftUI
 
 struct EventRowView: View {
   @Environment(\.colorScheme) var colorScheme
-//  @State var model: EventDetailModel
-//  let attendingUserNames: [String]
-//  @State var currentUser: EventAttendee
+  let logger = Logger(subsystem: "amStizzleReboot", category: "EventRowView")
+  //  @State var model: EventDetailModel
+  //  let attendingUserNames: [String]
+  @State var currentEventAttendee: EventAttendee? /*= EventAttendee(id: UUID(), eventId: UUID(), profileId: UUID(), attendanceStatus: 0, createdAt: Date.now, updatedAt: Date.now)*/
   let event: Event
   let currentUserId: UUID
-//  let groupColor: Color
-//  let onDetailTapped: () -> Void
+  @State var eventAttendees: [EventAttendee] = []
+  //  let groupColor: Color
+  //  let onDetailTapped: () -> Void
   
   var body: some View {
     VStack {
@@ -57,28 +59,38 @@ struct EventRowView: View {
         
         VStack {
           NavigationLink {
-           EventDetailView(event: event)
+            EventDetailView(event: event)
           } label: {
             TitleView
           }
-          TimeSection
-          //        }
           
-//          if event.attendees.contains(user.id) || event.notParticipating.contains(user.id) {
-//            AttendeesView
-//          } else {
-            HStack {
+          TimeSection
+          
+          HStack {
+            if currentEventAttendee?.attendanceStatus == 0 {
               ButtonView(event: event, userId: currentUserId, buttonType: .refuseButton, image: "xmark", text: "nope, i'm out")
               ButtonView(event: event, userId: currentUserId, buttonType: .attendButton, image: "checkmark", text: "am Stizzle!")
-//            }
+            } else if currentEventAttendee?.attendanceStatus == 1 {
+              NavigationLink {
+                EventDetailView(event: event)
+              } label: {
+                AttendanceView(currentEventAttendee: currentEventAttendee!,eventAttendees: eventAttendees, invitationState: .inviteAccepted, image: "checkmark.circle.fill", text: "amStizzle!")
+              }
+            } else if currentEventAttendee?.attendanceStatus == 2 {
+              NavigationLink {
+                EventDetailView(event: event)
+              } label: {
+                AttendanceView(currentEventAttendee: currentEventAttendee!, eventAttendees: eventAttendees, invitationState: .inviteDeclined, image: "xmark.circle.fill", text: "You declined")
+              }
+            }
           }
         }
         .padding()
       }
-      .frame(height: 300)
+      .frame(height: 270)
       .containerShape(.rect(cornerRadius: 60))
     }
-//    .padding()
+    //    .padding()
   }
   
   var TitleView: some View {
@@ -87,15 +99,20 @@ struct EventRowView: View {
         .fill(Color.blue.opacity(0.5))
         .shadow(color: .black.opacity(0.7), radius: 2, x: 2, y: 2)
         .shadow(color: .white.opacity(0.7), radius: 2, x: -2, y: -2)
-      Text(event.title ?? "Unknown")
+      Text(event.title ?? "Unknown Event")
         .minimumScaleFactor(0.5)
         .foregroundStyle(Color.primary)
         .font(.largeTitle)
         .fontWeight(.bold)
+//        .border(.cyan)
         .padding()
+//        .padding(.leading)
+//        .padding(.bottom, 2)
         .shadow(color: .black.opacity(0.7), radius: 1, x: 1, y: 1)
         .shadow(color: .white.opacity(0.7), radius: 1, x: -1, y: -1)
     }
+    .frame(height: 70)
+//    .border(.orange)
   }
   
   var TimeSection: some View {
@@ -107,12 +124,12 @@ struct EventRowView: View {
       HStack {
         Text(event.startDate?
           .formatted(date: .abbreviated, time: .omitted) ?? "N/A")
-          .minimumScaleFactor(0.5)
+        .minimumScaleFactor(0.5)
         //          .fontWidth(.condensed)
-          .font(.title)
-          .fontWeight(.bold)
-          .fontDesign(.rounded)
-          .foregroundStyle(Color.primary)
+        .font(.title)
+        .fontWeight(.bold)
+        .fontDesign(.rounded)
+        .foregroundStyle(Color.primary)
         
         Spacer()
         HStack {
@@ -128,93 +145,66 @@ struct EventRowView: View {
             Text(event.endDate?.formatted(date: .omitted, time: .shortened) ?? "N/A")
           }
           .minimumScaleFactor(0.8)
-//                  .frame(maxWidth: .infinity)
-//          .border(.orange, width: 1)
+          //                  .frame(maxWidth: .infinity)
+          //          .border(.orange, width: 1)
           .fontWeight(.black)
         }
-//        .border(.purple, width: 1)
+        //        .border(.purple, width: 1)
         .font(.title2)
       }
       .shadow(color: .black.opacity(0.7), radius: 1, x: 1, y: 1)
       .shadow(color: .white.opacity(0.7), radius: 1, x: -1, y: -1)
       .padding()
     }
-//    .padding(.vertical, 6)
+    //    .padding(.vertical, 6)
   }
-//  var AttendeesView: some View {
-//    ZStack(alignment: .topLeading) {
-//      ConcentricRectangle()
-//        .fill(event.attendees.contains(user.id) ? Color.green.opacity(0.4) : Color.red.opacity(0.4))
-//        .frame(minHeight: 90)
-//        .shadow(color: .black.opacity(0.7), radius: 2, x: 2, y: 2)
-//        .shadow(color: .white.opacity(0.7), radius: 2, x: -2, y: -2)
-//      VStack(alignment: .leading) {
-//        HStack {
-//          Text("Attendees:")
-//            .font(.headline)
-//          ForEach(event.attendees, id: \.self) { _ in
-//            ZStack {
-//              Circle()
-//                .fill(Color.gray)
-//              HStack {
-//                Text(currentUser.firstName.first!.uppercased() + currentUser.lastName.first!.uppercased())
-//              }
-//              .fontWeight(.bold)
-//              .fontDesign(.rounded)
-//              .foregroundStyle(Color.white)
-//              .fontWidth(.compressed)
-//            }
-//          }
-//        }
-//        HStack {
-//          Text("Not attending:")
-//            .font(.headline)
-//          ForEach(event.notParticipating, id: \.self) { _ in
-//            ZStack {
-//              Circle()
-//                .fill(Color.gray)
-//              HStack {
-//                Text(currentUser.firstName.first!.uppercased() + currentUser.lastName.first!.uppercased())
-//              }
-//              .fontWeight(.bold)
-//              .fontDesign(.rounded)
-//              .foregroundStyle(Color.white)
-//              .fontWidth(.compressed)
-//            }
-//          }
-//        }
-//      }
-//      .shadow(color: .black.opacity(0.7), radius: 1, x: 1, y: 1)
-//      .shadow(color: .white.opacity(0.7), radius: 1, x: -1, y: -1)
-//      .padding()
-//    }
-//  }
-//  func loadCurrentAttendee() async {
-//    do {
-//      let currentUser = try await Supabase.shared.auth.session.user
-//      
-////      let currentEventAttendee: EventAttendee =
-////      try await Supabase.shared
-////        .from("event_attendees")
-////        .select()
-////        .eq("profile_id", value: currentUser.id)
-////        .single()
-////        .execute()
-////        .value
-////      
-//////      logger.info("MyEventsCount: \(usersFetchedEvents.count)")
-////      
-////      currentEventAttendee
-////      await loadInvitedEvents()
-//    } catch {
-//      logger.error("\(error)")
-//    }
-//  }
+  
+  func loadCurrentAttendee() async {
+    do {
+      let currentUser = try await Supabase.shared.auth.session.user
+      
+      let currentEventAttendee: EventAttendee =
+      try await Supabase.shared
+        .from("event_attendees")
+        .select()
+        .eq("profile_id", value: currentUser.id)
+        .single()
+        .execute()
+        .value
+      //
+      logger.info("currentEventAttendeeId: \(currentEventAttendee.profileId?.uuidString ?? "no profileId")")
+      //
+      self.currentEventAttendee = currentEventAttendee
+      //      await loadInvitedEvents()
+    } catch {
+      logger.error("\(error)")
+    }
+  }
+  func loadEventAttendees() async {
+    do {
+      let fetchedEventAttendees: [EventAttendee] =
+      try await Supabase.shared
+        .from("event_attendees")
+        .select()
+      //        .eq("profile_id", value: $0.id)
+        .eq("event_id", value: event.id)
+        .execute()
+        .value
+      
+      logger.info("EventAttendeesCount: \(fetchedEventAttendees.count)")
+      
+      self.eventAttendees = fetchedEventAttendees
+      
+    } catch {
+      logger.error("\(error)")
+    }
+    
+  }
 }
 
 
 struct ButtonView: View {
-  let logger = Logger(subsystem: "amStizzleReboot", category: "EventRowView")
+  let logger = Logger(subsystem: "amStizzleReboot", category: "ButtonView")
   
   var event: Event
   var userId: UUID
@@ -235,9 +225,6 @@ struct ButtonView: View {
   var image: String
   var text: String
   
-  let attendColor: Color = .green.opacity(0.6)
-  let refuseColor: Color = .red.opacity(0.6)
-  
   var body: some View {
     Button {
       switch buttonType {
@@ -255,9 +242,9 @@ struct ButtonView: View {
             logger.error("\(error.localizedDescription)")
           }
         }
-//        event.attendees.append(user.id)
-//        print("attending: \(event.attendees)")
-//        return /*remove after commenting in*/
+        //        event.attendees.append(user.id)
+        //        print("attending: \(event.attendees)")
+        //        return /*remove after commenting in*/
         
       case .refuseButton:
         Task {
@@ -273,8 +260,8 @@ struct ButtonView: View {
             logger.error("\(error.localizedDescription)")
           }
         }
-//        event.notParticipating.append(user.id)
-//        print("notParticipating: \(event.notParticipating)")
+        //        event.notParticipating.append(user.id)
+        //        print("notParticipating: \(event.notParticipating)")
         return /*remove after commenting in*/
       }
     } label: {
@@ -302,11 +289,106 @@ struct ButtonView: View {
   }
 }
 
-#Preview {
-  NavigationStack {
-    let currentSampleUserId = UUID()
-    let event = Event(id: UUID(), title: "Test", details: nil, startDate: Date.now, endDate: Date.now + 3600, createdAt: Date.now, updatedAt: Date.now, creatorId: currentSampleUserId)
-    EventRowView(event: event, currentUserId: currentSampleUserId)
+struct AttendanceView: View {
+  let logger = Logger(subsystem: "amStizzleReboot", category: "AttendanceView")
+  
+  enum InvitationState {
+    case inviteAccepted
+    case inviteDeclined
+  }
+  var backgroundColor: Color {
+    switch invitationState {
+    case .inviteAccepted:
+      return Color.green.opacity(0.3)
+    case .inviteDeclined:
+      return Color.red.opacity(0.3)
+    }
+  }
+  
+  var currentEventAttendee: EventAttendee
+  var eventAttendees: [EventAttendee]
+  var invitationState: InvitationState
+  var image: String
+  var text: String
+  
+  var body: some View {
+    ZStack(alignment: .topLeading) {
+      ConcentricRectangle()
+        .fill(backgroundColor
+          .shadow(.inner(color: .white.opacity(0.7), radius: 1, x: -4, y: -4))
+          .shadow(.inner(color: .black.opacity(0.7), radius: 1, x: 4, y: 4))
+        )
+      VStack(alignment: .leading) {
+        Text(text)
+          .minimumScaleFactor(0.5)
+          .foregroundStyle(Color.primary)
+          .font(.title2)
+          .fontWeight(.bold)
+          .shadow(color: .black.opacity(0.7), radius: 1, x: 1, y: 1)
+          .shadow(color: .white.opacity(0.7), radius: 1, x: -1, y: -1)
+        HStack(spacing: -6) {
+          ForEach(eventAttendees.filter { $0.attendanceStatus == 1 }) { attendee in
+            //            ZStack {
+            //              Circle()
+            //                .fill(Color.gray)
+            //              HStack {
+            //                Text(currentUser.firstName.first!.uppercased() + currentUser.lastName.first!.uppercased())
+            //              }
+            //              .fontWeight(.bold)
+            //              .fontDesign(.rounded)
+            //              .foregroundStyle(Color.white)
+            //              .fontWidth(.compressed)
+            //            }
+            Image(systemName: "person.circle")
+              .minimumScaleFactor(0.5)
+              .foregroundStyle(attendee.profileId == currentEventAttendee.profileId ? Color.orange : Color.primary)
+              .font(.title2)
+              .fontWeight(.bold)
+              .shadow(color: .black.opacity(0.7), radius: 1, x: 1, y: 1)
+              .shadow(color: .white.opacity(0.7), radius: 1, x: -1, y: -1)
+          }
+          Spacer()
+          ForEach(eventAttendees.filter { $0.attendanceStatus == 2 }) { attendee in
+            //            ZStack {
+            //              Circle()
+            //                .fill(Color.gray)
+            //              HStack {
+            //                Text(currentUser.firstName.first!.uppercased() + currentUser.lastName.first!.uppercased())
+            //              }
+            //              .fontWeight(.bold)
+            //              .fontDesign(.rounded)
+            //              .foregroundStyle(Color.white)
+            //              .fontWidth(.compressed)
+            //            }
+            Image(systemName: "person.circle")
+              .minimumScaleFactor(0.5)
+              .foregroundStyle(attendee.profileId == currentEventAttendee.profileId ? Color.mint : Color.primary)
+              .font(.title2)
+              .fontWeight(.bold)
+              .shadow(color: .black.opacity(0.7), radius: 1, x: 1, y: 1)
+              .shadow(color: .white.opacity(0.7), radius: 1, x: -1, y: -1)
+          }
+        }
+      }
+      .padding()
+    }
   }
 }
 
+#Preview {
+  NavigationStack {
+    let currentSampleUserId = UUID()
+    let currentSampleAttendee = EventAttendee(id: UUID(), eventId: UUID(), profileId: currentSampleUserId, attendanceStatus: 1, createdAt: Date.now, updatedAt: Date.now)
+    let event = Event(id: UUID(), title: "Test", details: nil, startDate: Date.now, endDate: Date.now + 3600, createdAt: Date.now, updatedAt: Date.now, creatorId: currentSampleUserId)
+    let sampleEventAttendees = [
+      currentSampleAttendee,
+      EventAttendee(id: UUID(), eventId: event.id, profileId: UUID(), attendanceStatus: 1, createdAt: Date.now, updatedAt: Date.now),
+      EventAttendee(id: UUID(), eventId: event.id, profileId: UUID(), attendanceStatus: 1, createdAt: Date.now, updatedAt: Date.now),
+      EventAttendee(id: UUID(), eventId: event.id, profileId: UUID(), attendanceStatus: 1, createdAt: Date.now, updatedAt: Date.now),
+      EventAttendee(id: UUID(), eventId: event.id, profileId: UUID(), attendanceStatus: 2, createdAt: Date.now, updatedAt: Date.now),
+      EventAttendee(id: UUID(), eventId: event.id, profileId: UUID(), attendanceStatus: 2, createdAt: Date.now, updatedAt: Date.now),
+      EventAttendee(id: UUID(), eventId: event.id, profileId: UUID(), attendanceStatus: 2, createdAt: Date.now, updatedAt: Date.now)
+    ]
+    EventRowView(currentEventAttendee: currentSampleAttendee, event: event, currentUserId: currentSampleUserId, eventAttendees: sampleEventAttendees)
+  }
+}
